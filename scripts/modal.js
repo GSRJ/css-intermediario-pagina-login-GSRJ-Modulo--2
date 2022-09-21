cd9b2c034795284a0fb75b100e264f6f3634550d
Gerson S. Rocha Jr <gerson.contato@outlook.com>
2022-09-21 16:41:15 -0300

    Entrega finalizada

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,9 @@
-/* Desenvolva seu código aqui... */
+const buttonModal = document.querySelectorAll("[data-control-modal]");
+
+for (let index = 0; index < buttonModal.length; index++) {
+  buttonModal[index].addEventListener("click", function () {
+    let valueModalControl =
+      buttonModal[index].getAttribute("data-control-modal");
+    document.getElementById(valueModalControl).classList.toggle("showModal");
+  });
+}
